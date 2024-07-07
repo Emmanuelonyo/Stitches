d667294dd54912aa8f88a4c8c730e6e10f290b06
Oluwakemi Mafe <oluwakemimafe@MacBook-Pro.local>
2024-07-07 17:13:22 -0400

    Completed form validation and content loading. Also updated some styles

--- js/index.js
+++ js/index.js
@@ -115,18 +115,130 @@ var data = {
             imageSrc: "../img/tshirt.jpeg",
             productName: "T Shirt",
             productDescription: "Original by Stitches"
+        },
+        {
+            imageSrc: "../img/tshirt.jpeg",
+            productName: "T Shirt",
+            productDescription: "Original by Stitches"
         }
-        
     ]
 } 
 
-document.addEventListener('DOMContentLoaded', populateEvents())
+document.addEventListener("DOMContentLoaded", populateContent);
+
+// Add active class to nav items
+function styleNavItems(){
+    //get all navigation items
+    const navItems = document.querySelectorAll('#navItemsCont a');
+
+    //get the current page URL
+    const currentPage = window.location.pathname;
+
+    //loop through each nav item
+    navItems.forEach(function(navItem) {
+        //if the nav item's href matches the current page URL, add the 'active' class
+        if (navItem.getAttribute('href') === currentPage) {
+            navItem.classList.add('active');
+        }
+    });
+}
+
+// Load data for each page when the user navigates to that page
+function populateContent(){
+    //style navigation menu items
+    styleNavItems();
+
+    // load products when user is on the showcase page
+    if (window.location.pathname.endsWith("/html/showcase.html")) {
+        loadProducts();
+    } 
+    
+    // add submit eventlistener on the appointment form
+    if (window.location.pathname.endsWith("/html/inquiries.html")) {
+        validateAndSubmitForm();
+    } 
+    
+    // load events when user is on the events page
+    if (window.location.pathname.endsWith("/html/events.html")) {
+        loadEvents();
+    } 
+}
+
+// Validate and submit form 
+function validateAndSubmitForm(){
+   //get appointment form
+    const apptForm = document.getElementById(id="appointment-form");
+    const alert = document.getElementById("customAlert");
+    const alertMessage = document.getElementById("alert-message");
+    const span = document.getElementsByClassName("close")[0];
+    
+    //add submit event listener
+    apptForm.addEventListener("submit", function(event) {
+        //prevent form from submitting before validation
+        event.preventDefault();
+        
+        //get appointment form field values
+        const name = document.getElementById("name").value.trim();
+        const email = document.getElementById("email").value.trim();
+        const apptDate = document.getElementById("date").value;
+        const message = document.getElementById("message").value.trim();
+        
+        // Validate each field
+        let formIsValid = true;
+        let errorMessage = "";
+
+        if (name.length == 1 ) {
+            formIsValid = false;
+            errorMessage += "Name must have at least two letters.\n";
+
+        if (!validateEmail(email)) {
+            formIsValid = false;
+            errorMessage += "Email is not valid.\n";
+        }
+
+        if (message.length <= 10) {
+            formIsValid = false;
+            errorMessage += "Message must include at least 10 letters.\n";
+        }
+
+        if (formIsValid) {
+            alertMessage.innerHTML = "Scheduling an appointment for " + name + "...";
+            alert.style.display = "block";
+            
+            //submit form
+            apptForm.submit();
+            
+            alertMessage.innerHTML = "Appointment successfully scheduled for" + apptDate + ".<br>See you then!";
+            alert.style.display = "block";
+        } else {
+            alertMessage.innerHTML = "We are unnable to schedule your appointment because of the following issues:<br><br>" + errorMessage.replace(/\n/g, "<br>") + "<br>Kindly address them and try again.";
+            alert.style.display = "block";
+        }
+    }
+    });
+    
+     span.onclick = function() {
+        alert.style.display = "none";
+    }
+
+    window.onclick = function(alert) {
+        if (event.target == alert) {
+            alert.style.display = "none";
+        }
+    }
+}
+
+ // Check if email is fromatted properly   
+ function validateEmail(email) {
+    // email validation regex
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
 
 // Parse event data
-function populateEvents(){
+function loadEvents(){
     let eventsData = data.events;
-    console.log(eventsData.length);
-    
+
     //get the main events container
     let mainEventContainer = document.getElementById(id="event-list");
 
@@ -135,21 +247,20 @@ function populateEvents(){
         eventsDiv.classList.add("events"); // add class name events to the outer div
 
         let dateIconContDiv = document.createElement("div"); //create div for date icon content
-        dateIconContDiv.classList.add("date-icon-cont"); // add class name events to the date icon content div
+        dateIconContDiv.classList.add("date-icon-cont"); // add class name date-icon-cont to the date icon content div
         let dateIconDiv = document.createElement("div"); //create date icon div
-        dateIconDiv.classList.add("date-icon"); // add class name events to the date icon div
+        dateIconDiv.classList.add("date-icon"); // add class name date-icon to the date icon div
         let dateDiv = document.createElement("div"); //create div for date 
-        dateDiv.classList.add("date-number"); // add class name events to the date div
+        dateDiv.classList.add("date-number"); // add class name date-number to the date div
         let monthDiv = document.createElement("div"); //create div for month 
-        monthDiv.classList.add("date-month"); // add class name events to the month div
+        monthDiv.classList.add("date-month"); // add class name date-month to the month div
 
         let eventInfoContDiv = document.createElement("div"); //create event info content div
-        eventInfoContDiv.classList.add("event-info-content"); // add class name events to the date icon div
-        eventInfoContDiv.style.backgroundColor = "greenyellow"; //set background color
+        eventInfoContDiv.classList.add("event-info-content"); // add class name event-info-content to the event info content div
         let eventNameDiv = document.createElement("div"); //create div for event name 
-        eventNameDiv.classList.add("event-name"); // add class name events to the date div
+        eventNameDiv.classList.add("event-name"); // add class name event-name to the event name div
         let eventPeriodDiv = document.createElement("div"); //create div for event period 
-        eventPeriodDiv.classList.add("event-period"); // add class name events to the month div
+        eventPeriodDiv.classList.add("event-period"); // add class name event-period to the event peiod div
 
         //append elements
         dateIconDiv.appendChild(dateDiv);
@@ -176,4 +287,44 @@ function populateEvents(){
         mainEventContainer.appendChild(eventsDiv);
     });
     
+
+
+
+}
+
+// Parse product data
+function loadProducts(){
+    let productsData = data.products;
+
+    //get the main products container
+    let mainProductContainer = document.getElementById(id="productList");
+
+    productsData.forEach(product => {
+        let productDiv = document.createElement("div"); //create outer div
+        productDiv.classList.add("product"); // add class name product to the outer div
+
+        let imgDiv = document.createElement("div"); //create div for image
+        imgDiv.classList.add("productImage"); // add class name productImage to the img div
+        let nameDiv = document.createElement("div"); //create product name div
+        nameDiv.classList.add("productName"); // add class name productName to the date icon div
+
+
+        let img = document.createElement("img"); // create image element
+        img.src = product.imageSrc; // add image source
+        img.alt = product.productDescription; // add image alt text
+        let span = document.createElement("span"); //create span for description
+        span.classList.add("productDesc"); // add class name productDesc to the span
+        span.textContent = product.productDescription
+
+        //append elements
+        imgDiv.appendChild(img);
+        nameDiv.innerHTML = product.productName;
+        nameDiv.appendChild(span);
+        
+        productDiv.appendChild(imgDiv);
+        productDiv.appendChild(nameDiv);
+      
+        //append product to the main container
+        mainProductContainer.appendChild(productDiv);
+    });
 }
